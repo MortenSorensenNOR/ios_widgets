b5a0d5858e959a40efd5c8c0a92061555bc70be3
MortenSorensenNOR <morten.sorensen.04@gmail.com>
2023-09-15 20:20:58 +0200

    Updated timetable widget

--- timeplan/timeplan.js
+++ timeplan/timeplan.js
@@ -1,1 +1,19 @@
-// TODO: Make widget
+/* 
+
+Script for extracting the timetable data from https://ntnu.1024.no/USER
+
+*/
+
+const timetable = document.getElementById("schedule");
+const tt_tbody = timetable.getElementsByTagName("tbody")[0];
+const tt_rows = tt_tbody.getElementsByTagName("tr");
+
+let schedule = {
+
+}
+
+for (let row in tt_rows) {
+    const cols = row.getElementsByTagName("td");
+    const time = cols[0].children[0].innerHTML;
+    console.log(time)
+}
