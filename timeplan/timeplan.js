4dbfdd3d12e793073a2d11974d7ac9cefd84baab
MortenSorensenNOR <morten.sorensen.04@gmail.com>
2023-09-15 20:29:31 +0200

    Test

--- timeplan/timeplan.js
+++ timeplan/timeplan.js
@@ -4,16 +4,20 @@ Script for extracting the timetable data from https://ntnu.1024.no/USER
 
 */
 
-const timetable = document.getElementById("schedule");
-const tt_tbody = timetable.getElementsByTagName("tbody")[0];
-const tt_rows = tt_tbody.getElementsByTagName("tr");
-
-let schedule = {
-
-}
-
-for (let row in tt_rows) {
-    const cols = row.getElementsByTagName("td");
-    const time = cols[0].children[0].innerHTML;
-    console.log(time)
-}
+(function() {
+    const timetable = document.getElementById("schedule");
+    const tt_tbody = timetable.getElementsByTagName("tbody")[0];
+    const tt_rows = tt_tbody.getElementsByTagName("tr");
+    
+    let schedule = {
+    
+    }
+    let test = []
+    
+    for (let row in tt_rows) {
+        const cols = row.getElementsByTagName("td");
+        const time = cols[0].children[0].innerHTML;
+        test.push(time)
+    }
+    return test
+})
